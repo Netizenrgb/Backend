104f0ac8ce19c0d0d817365019393fdd6ad534b0
Netizenrgb <2006ajitsharma@gmail.com>
2026-09-01 15:45:22 +0530

    Delete db directory

--- db/server.js
+++
@@ -1,7 +0,0 @@
-// this is the server/index file consist only the connection MVC archi
-
-// connection
-const app = require("./src/app");
-app.listen(3000, () => {
-  console.log("This is working on the port 3000 ");
-});
